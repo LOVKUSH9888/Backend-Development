8f3a26f345e8936296bc3b1a927cb81f687bf184
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-30 18:37:44 +0530

    HOF and Xall Back Function

--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -13,3 +13,17 @@ app.listen(3000, () => {
 
 //revision
 //solving - HaxkerEarth-Airbus ideathon  + Namha Yatri - 29.4.23 @ 8.46pm
+
+//😁😀😁😀😁😂😁😀😁😂
+
+//😡 Higher Order Function = Function which takes another function as an argument = Calc
+function calc(number1, number2, fn){
+	return fn(number1, number2);
+}
+//😀 Callback Function = function which get passed as an argument to another function = fn
+
+return fn(number1, number2);
+
+//😁😀😁😀😁😂😁😀😁😂 
+
+
