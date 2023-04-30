c16bffcc50deedb1839633f5bf2fb876a7d1c41b
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-30 18:25:24 +0530

    Creating the server and listening the port

--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -1,22 +1,15 @@
 const express = require('express')
-const app = express()
+const app = express();
 
-app.listen (3000, function () {
-    console.log("server started");
-});
 
-// const port = 3000
-
-/*
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log("Port are working");
 })
 
-*/
 
 //revision
 //solving - HaxkerEarth-Airbus ideathon  + Namha Yatri - 29.4.23 @ 8.46pm
