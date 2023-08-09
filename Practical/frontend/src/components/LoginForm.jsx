fe653915676eafbc7558f3376f4157d5c4de28e6
LOVKUSH <lovkushyadav9888@gmail.com>
2023-08-09 22:49:19 +0530

    Creating my own CRUD app

---
+++ Practical/frontend/src/components/LoginForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function LoginForm() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // You can add code to send the login data to the server
+    // For now, we'll just log a message
+    console.log('Entered Username:', name);
+    console.log('Entered Password:', password);
+    console.log("Login successful");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br></br>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br></br>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default LoginForm;
